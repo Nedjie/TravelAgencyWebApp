dc5ea932bbd1176e6bedd4adc3a46e3ccf9c7539
Неджмие Чакър <122801787+Nedjie@users.noreply.github.com>
2024-12-04 17:12:35 +0200

    Deleting Files that not used in project

--- obj/Debug/net8.0/TravelAgencyWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/TravelAgencyWebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelAgencyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de66701e4d4ccd4562fd4b8b72a1a93072bb0a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54367adba7f884ebb93ec7d4801e9634b42ac18d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelAgencyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelAgencyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
